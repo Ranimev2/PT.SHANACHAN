942f63612b5fed0de4a239d6087f456775a8e053
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-04-04 10:48:20 +0700

    Titit lu

--- serviceworker.js
+++ serviceworker.js
@@ -4,6 +4,7 @@ self.addEventListener('install', event => {
       return cache.addAll([
         '/',
         '/index.html',
+        '/manifest.json',
         '/favicon.ico',
         '/favicon.jpg',
         '/style.css'
