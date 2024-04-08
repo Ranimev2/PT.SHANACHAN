6a1ee965585b8277488c9318bf4a829abd705627
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-04-09 06:38:16 +0700

    Delete app/takamiyanime/Api.js

--- app/takamiyanime/Api.js
+++
@@ -1,8 +0,0 @@
-// URL untuk data anime berdasarkan genre
-const animeByGenreURL = "https://otakudesu-zeta.vercel.app/api/otakudesu/genres";
-
-// URL untuk bookmark anime
-const bookmarkAnimeURL = "https://kumanimeapi.vercel.app/api/anime/";
-
-// URL untuk berita terbaru tentang anime
-const animeNewsURL = "https://consumet-api-pink.vercel.app/news/ann/recent-feeds";
