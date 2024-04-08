dea4effdc0407205174e5597008ef84345fd2cb2
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-04-08 14:07:50 +0700

    Update register.js

--- App/js/register.js
+++ App/js/register.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
     if ('serviceWorker' in navigator && navigator.onLine) {
-        navigator.serviceWorker.register('/App/service-worker.js')
+        navigator.serviceWorker.register('/sw.js')
         .then((reg) => {
             console.log('Registrasi service worker Berhasil', reg);
         }, (err) => {
