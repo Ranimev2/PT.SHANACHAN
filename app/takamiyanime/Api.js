a601270459bd2b9459115808cb6e679f2d432a22
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-04-09 06:28:02 +0700

    Create Api.js

---
+++ app/takamiyanime/Api.js
@@ -0,0 +1,8 @@
+// URL untuk data anime berdasarkan genre
+const animeByGenreURL = "https://otakudesu-zeta.vercel.app/api/otakudesu/genres";
+
+// URL untuk bookmark anime
+const bookmarkAnimeURL = "https://api.example.com/bookmark/anime";
+
+// URL untuk berita terbaru tentang anime
+const animeNewsURL = "https://consumet-api-pink.vercel.app/news/ann/recent-feeds";
